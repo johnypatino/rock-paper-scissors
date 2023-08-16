810a1adf7e9e6c614af6fa41f0499770416d87ae
Jonathan <johnypatino99@gmail.com>
2023-08-16 19:59:42 -0400

    added ui

--- script.js
+++ script.js
@@ -13,35 +13,68 @@ function getComputerChoice() {
     return choice;
 }
 function playRound(computerSelection, playerSelection) {
+    
     let player = playerSelection.toLowerCase();
     if (computerSelection == player) {
-        return ("Tied! You both chose " + player);
+        x.textContent =  'Tied! You both chose ' + player
     }
     else if (computerSelection == 'rock' && player == 'scissors') {
-        return('You lost! ' + computerSelection + ' beats ' + player)
+        cscore++;
+        x.textContent = 'You lost! Rock beats Scissors';
     }
     else if (computerSelection == 'paper' && player == 'rock') {
-        return ('You lost! ' + computerSelection + ' beats ' + player)
+        cscore++;
+        x.textContent ='You lost! Paper beats Rock';
     } 
      else if (computerSelection == 'scissors' && player == 'paper') {
-         return('You lost! ' + computerSelection + ' beats ' + player)
+        cscore++;
+        x.textContent ='You lost! Scissors beats Paper';
     }
     else if (computerSelection == 'scissors' && player == 'rock') {
-        return ('You Win! ' + player + ' beats ' + computerSelection)
+        pscore++;
+        x.textContent = 'You Win! Scissors beats Rock';
     }
     else if (computerSelection == 'rock' && player == 'paper') {
-        return ('You Win! ' + player + ' beats ' + computerSelection)
+        pscore++;
+        x.textContent = 'You Win! Rock beats Paper';
     }
     else if (computerSelection == 'paper' && player == 'scissors') {
-        return ('You Win! ' + player + ' beats ' + computerSelection)
+        pscore++;
+        x.textContent ='You Win! Paper beats Scissors!';
     }
-}
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("rock, paper, scissors?");
-        let computerSelection = getComputerChoice();
-         console.log(playRound(computerSelection, playerSelection));
+    if (pscore >= 5) {
+        x.textContent = 'You won 5 times, you are the winner'
         
     }
+    if (cscore >= 5) {
+        x.textContent = 'You lost 5 times, you are the Loser'
+    }
+    playerScore.textContent = 'Player Score: ' + pscore;
+    computerScore.textContent = 'Computer Score: ' + cscore;
+    scoresdiv.appendChild(playerScore);
+    scoresdiv.appendChild(computerScore);
+    container.appendChild(x);
+    console.log(player);
 }
-game();
+const x = document.createElement('p');
+x.style.textAlign = 'center';
+const container = document.querySelector('#result');
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', ()=> {
+
+        let player = button.id;
+        const play = document.createElement('p');
+        let computerSelection = getComputerChoice();
+        playRound(computerSelection, player);
+        
+    })
+});
+let pscore = 0;
+let cscore = 0;
+const scoresdiv = document.querySelector('#score');
+const playerScore = document.createElement('h5');
+playerScore.style.color ='blue';
+const computerScore = document.createElement('h5')
+computerScore.style.color = 'red';
+
